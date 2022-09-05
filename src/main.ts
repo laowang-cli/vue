9f61611cd60bfdea70c74036e4736d7f87d866b5
bin.wang <bin.wang1@za.group>
2022-09-05 16:34:37 +0800

    feat: add tailwind support

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue"
 import "element-plus/dist/index.css"
 import router from "./router"
 import store from "./store"
+import "./assets/stylesheets/index.scss"
 
 createApp(App)
   .use(store)
