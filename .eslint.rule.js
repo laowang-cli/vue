ceb3f5a9bf27be165c881ae0e90ce6f0119292e4
wangbin <imwangbinvip@gmail.com>
2022-09-24 15:26:59 +0800

    clear

--- .eslint.rule.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  quotes: [2, "double"],
-  "@typescript-eslint/no-explicit-any": [0]
-}
--- src/@types/enums/ResponseErrorType.ts
+++
@@ -1,5 +0,0 @@
-export enum ErrorType {
-  MESSAGE, // 上方提醒
-  NOTIFY, // 右上角提醒
-  DIALOG // 弹框提醒
-}
